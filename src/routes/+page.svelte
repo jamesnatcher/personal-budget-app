0c525a4e9bf134a8400c65aca5901b6759765154
jamesnatcher <jamesnatcher@gmail.com>
2023-12-14 15:01:18 -0900

    App initalization
    - Initialized app
    - Added Drizzle-ORM
    - Added Tailwindcss

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    console.log("DEV");
+</script>
+
+<h1 id="mainheader">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
