acff30c1be8f6cb04b6ac34b23cee3c673b53c71
jamesnatcher <jamesnatcher@gmail.com>
2023-12-19 15:15:24 -0900

    Fixes to the redirect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,8 +17,6 @@
 
 <a href="/auth"> Get Started </a>
 
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.gray.100);
